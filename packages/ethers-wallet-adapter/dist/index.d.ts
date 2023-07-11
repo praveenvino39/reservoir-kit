fa64fe8deb719ec3769b44a750db1299bf3b6e01
praveenvino39 <praveena4e@gmail.com>
2023-07-11 19:59:32 +0530

    Dist removed

--- packages/ethers-wallet-adapter/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ReservoirWallet } from "@reservoir0x/reservoir-sdk";
-import { Signer } from "ethers/lib/ethers";
-export const adaptEthersSigner: (signer: Signer) => ReservoirWallet;
-
-//# sourceMappingURL=index.d.ts.map
